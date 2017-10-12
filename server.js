99c33830a3d9917385764ec46046584257aab873
Emily Sun <emilysun@Emilys-MBP.fios-router.home>
2017-10-12 13:32:35 -0400

    added changes to CSS

--- server.js
+++ server.js
@@ -19,14 +19,17 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:"application/vnd.api+json"}));
 //makes sure I can read my css files
-app.use(express.static(path.join(__dirname, './app/public')));
+app.use(express.static(path.join(__dirname, './app')));
 
 
 //================================
 //Router: points to route files
 // require("./app/routing/apiRoutes")(app);
 // require("./app/routing/htmlRoutes")(app);
-
+//Default USE route goes to home
+	app.use(function(req, res) {
+	   res.sendFile(path.join(__dirname, "/../app/index.html"));
+	});
 
 //================================
 //Listener
